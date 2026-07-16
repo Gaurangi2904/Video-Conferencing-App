7b73ec22bae4841bacb0cfa5ae96f66ba5773b4a
Gaurangi2904 <kaparegaurangi@gmail.com>
2026-07-16 13:18:55 +0530

    Updated frontend environment configuration

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,3 +1,7 @@
-const server = "http://localhost:8000";
+let IS_PROD = true;
+const server = IS_PROD ?
+     "https://video-conferencing-backend-gys7.onrender.com" :
 
+     "http://localhost:8000"
+    
 export default server;
